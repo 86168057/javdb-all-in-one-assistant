73f1b4cd1fe6d698c4ec34208f233c41d3f69a70
86168057 <86168057@users.noreply.github.com>
2026-01-26 21:45:17 +0800

    Add auto-update URLs

--- javdb-emby-checker.user.js
+++ javdb-emby-checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAVDB全能助手 V0.1
 // @namespace    http://tampermonkey.net/
-// @version      0.1.6
+// @version      0.1.7
 // @description  JAVDB + EMBY 联动脚本：实时同步入库状态、预览图查看、磁力链管理、多站点搜索
 // @author       by：潇洒公子
 // @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTYgMjU2Ij48ZGVmcz48bGluZWFyR3JhZGllbnQgaWQ9ImEiIHgxPSIwJSIgeTE9IjAlIiB4Mj0iMTAwJSIgeTI9IjEwMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0eWxlPSJzdG9wLWNvbG9yOiMwMGFjZWE7c3RvcC1vcGFjaXR5OjEiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0eWxlPSJzdG9wLWNvbG9yOiM1MmJlODA7c3RvcC1vcGFjaXR5OjEiLz48L2xpbmVhckdyYWRpZW50PjwvZGVmcz48cmVjdCB3aWR0aD0iMjU2IiBoZWlnaHQ9IjI1NiIgZmlsbD0idXJsKCNhKSIvPjx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBmb250LWZhbWlseT0iQXJpYWwsIHNhbnMtc2VyaWYiIGZvbnQtc2l6ZT0iMTIwIiBmb250LXdlaWdodD0iYm9sZCIgZmlsbD0iI2ZmZiIgdGV4dC1hbmNob3I9Im1pZGRsZSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSI+SkQ8L3RleHQ+PC9zdmc+
@@ -22,8 +22,9 @@
 // @connect      172.16.0.0/12
 // @run-at       document-idle
 // @license      MIT
-// @homepage     https://greasyfork.org/zh-CN/scripts/
-// @supportURL   https://greasyfork.org/zh-CN/scripts/
+// @updateURL    https://raw.githubusercontent.com/86168057/javdb-all-in-one-assistant/main/javdb-emby-checker.user.js
+// @downloadURL  https://raw.githubusercontent.com/86168057/javdb-all-in-one-assistant/main/javdb-emby-checker.user.js
+// @homepage     https://github.com/86168057/javdb-all-in-one-assistant
 // ==/UserScript==
 
 (function() {
